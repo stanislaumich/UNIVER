0aa71e6b974ed362796aed6e8d66553ba692899e
Станислав Микулич <staslz42stas@gmail.com>
2023-01-12 10:12:33 +0300

    AUTO FROM WORK 12.01.2023 10:12:32,94

---
+++ Windows-ADMIN.cmd
@@ -0,0 +1,9 @@
+rem https://bafista.ru/aktivacziya-microsoft-windows-i-office/
+rem kms.srv.crsoo.com kms.digiboy.ir kms.03k.org
+rem hq1.chinancce.com kms.shuax.com kms.lotro.cc kms789.com
+
+
+slmgr.vbs -ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms789.com
+slmgr /ato
+
