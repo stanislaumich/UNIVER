18ee569cbbcdcf2bfc604e27bbf40aeaed6ba77f
stanislaumich <staslz42stas.com>
2023-12-14 02:07:37 +0300

    AUTO FROM HOME 14.12.2023  2:07:37,16

--- up to inet - push.bat
+++ up to inet - push.bat
@@ -3,7 +3,7 @@ rem git config --global --add safe.directory 'S:/Контрольные/UNIVER'
 git add .
 git commit -m "AUTO FROM %place% %date% %time%"
 git config --global http.version HTTP/1.1
-git push
-rem git push origin --force
+rem git push
+git push origin --force
 git config --global http.version HTTP/2
 pause
